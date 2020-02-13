922c6188d1502905b95df9182006e67d01f9b43d
dAVE Inden <davidinden@gmail.com>
2020-02-13 12:33:19 -0800

    update Player class to work with items

--- src/player.py
+++ src/player.py
@@ -5,6 +5,23 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
 
     def update_room(self, room):
         self.current_room = room
+
+    def add_item(self, item):
+        self.inventory.append(item)
+        item.on_take()
+        return True
+
+    def list_inventory(self):
+        return self.inventory
+
+    def remove_item(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            item.on_drop()
+            return True
+        else:
+            return False

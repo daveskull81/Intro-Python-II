407a790ff97f596228062230fc07f4d543bc7a8c
dAVE Inden <davidinden@gmail.com>
2020-02-13 12:33:36 -0800

    update Room class to work with items

--- src/room.py
+++ src/room.py
@@ -2,14 +2,29 @@
 # description attributes.
 
 class Room:
-    n_to = None
-    s_to = None
-    e_to = None
-    w_to = None
-
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.items = []
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def add_item(self, item):
+        self.items.append(item)
+        return True
+    
+    def list_items(self):
+        return self.items
+
+    def remove_item(self, item):
+        if item in self.items:
+            self.items.remove(item)
+            return True
+        else:
+            return False
+
 
     def __str__(self):
         return F"Name: {self.name}\nDescription: {self.description}"
